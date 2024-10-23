4d45bbb02e48b547022d399b2c14203bc26748fa
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-10-22 19:07:21 -0500

    sd

--- src/pages/admin/components/navbar.js
+++ src/pages/admin/components/navbar.js
@@ -2,7 +2,7 @@ export default function NabAdmin(props) {
   return (
     <div>
       <ul className="flex m-10">
-        <li className="mr-5">
+        <li className="mr-4">
           <a
             href="/admin/product"
             className="px-5 py-2.5 rounded-full text-white text-sm tracking-wider font-medium border border-current outline-none bg-blue-500 hover:bg-blue-800 active:bg-blue-700"
@@ -10,11 +10,11 @@ export default function NabAdmin(props) {
             Productos
           </a>
         </li>
-        ||
+        /
         <li>
           <a
             href="/admin/categorias"
-            className="px-5 py-2.5 rounded-full text-white text-sm tracking-wider font-medium border border-current outline-none bg-purple-500 hover:bg-purple-800 ml-5"
+            className="px-5 py-2.5 rounded-full text-white text-sm tracking-wider font-medium border border-current outline-none bg-purple-500 hover:bg-purple-800 ml-4"
           >
             Categorias
           </a>
